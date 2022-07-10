e07f7a88b4fc20afa2ab2663efc6bfeba7cfc82c
dione-b <dionenassau321@gmail.com>
2022-07-10 16:15:55 -0300

    v.1.1.1

--- src/App.js
+++ src/App.js
@@ -1,109 +1,185 @@
-import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import "./styles/App.css";
-import twitterLogo from "./assets/twitter-logo.svg";
-const TWITTER_HANDLE = "Web3dev_";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
-const TOTAL_MINT_COUNT = 50;
+import React, { useEffect, useState } from "react"
+import "./styles/App.css"
+import twitterLogo from "./assets/twitter-logo.svg"
+import { ethers } from "ethers"
+import myEpicNft from "./utils/MyEpicNFT.json"
+
+// endereço do contrato
+const CONTRACT_ADDRESS = "0xa19DfB2Bf7cbF264D3Cd0317184C7b1a4126952c"
+
+// Constants
+const TWITTER_HANDLE = "Diiibastos"
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+const OPENSEA_LINK = `https://testnets.opensea.io/assets/rinkeby/${CONTRACT_ADDRESS}/`
+const TOTAL_MINT_COUNT = 50
+
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
+  /*
+   * Só uma variável de estado que usamos pra armazenar nossa carteira pública. Não esqueça de importar o useState.
+   */
+  let totalMinted = 0;
+  const [currentAccount, setCurrentAccount] = useState("")
+  const [mintTotal, setMintTotal] = useState(totalMinted)
+
   const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
+    /*
+     * Primeiro tenha certeza que temos acesso a window.ethereum
+     */
+    const { ethereum } = window
     if (!ethereum) {
       console.log("Certifique-se que você tem metamask instalado!")
-      return;
+      return
     } else {
       console.log("Temos o objeto ethereum!", ethereum)
     }
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+    /*
+     * Checa se estamos autorizados a carteira do usuário.
+     */
+    const accounts = await ethereum.request({ method: "eth_accounts" })
+    /*
+     * Usuário pode ter múltiplas carteiras autorizadas, nós podemos pegar a primeira que está lá!
+     */
     if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account:", account);
-      setCurrentAccount(account);
+      const account = accounts[0]
+      console.log("Encontrou uma conta autorizada:", account)
+      setCurrentAccount(account)
+
+      // Setup listener! Isso é para quando o usuário vem no site
+      // e já tem a carteira conectada e autorizada
+      setupEventListener()
     } else {
-      console.log("No authorized account found");
+      console.log("Nenhuma conta autorizada foi encontrada")
     }
-  };
-  /*
-   * Implemente seu método connectWallet aqui
-   */
+  }
   const connectWallet = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
       if (!ethereum) {
-        alert("Baixe o Metamask!");
-        return;
+        alert("Baixe o Metamask!")
+        return
       }
       /*
        * Método chique para pedir acesso a conta.
        */
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
-      });
+      })
       /*
        * Boom! Isso deve escrever o endereço público uma vez que autorizar o Metamask.
        */
-      console.log("Conectado", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      console.log("Conectado", accounts[0])
+      setCurrentAccount(accounts[0])
 
-  const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "INSIRA_O_ENDEREÇO_DO_CONTRATO_NA_REDE_RINKEBY";
+      // Setup listener! Para quando o usuário vem para o site
+      // e conecta a carteira pela primeira vez
+      setupEventListener()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getTotalNFTsMintedSoFar = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const connectedContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicNft.abi,
+      provider
+    );
+  let count = await connectedContract.getTotalNFTsMintedSoFar();
+  const total = parseInt(count._hex.substring(2), 16);
+  console.log("minted", total);
+  setMintTotal(total);
+};
+
+  // Setup do listener.
+  const setupEventListener = async () => {
+    // é bem parecido com a função
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
+
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(
-          CONTRACT_ADDRESS,
-          myEpicNft.abi,
-          signer
-        );
-        console.log("Vai abrir a carteira agora para pagar o gás...");
-        let nftTxn = await connectedContract.makeAnEpicNFT();
-        console.log("Cunhando...espere por favor.");
-        await nftTxn.wait();
-        console.log(
-          `Cunhado, veja a transação: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
-        );
+        // mesma coisa de novo
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer)
+
+        // Aqui está o tempero mágico.
+        // Isso essencialmente captura nosso evento quando o contrato lança
+        // Se você está familiar com webhooks, é bem parecido!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          alert(
+            `Ei! Nós cunhamos seu NFT e o enviamos para sua carteira. Pode estar em branco agora. Pode levar no máximo 10 minutos para aparecer no OpenSea. Aqui está o link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        })
+
+        console.log("Setup event listener!")
       } else {
-        console.log("Objeto ethereum não existe!");
+        console.log("Objeto ethereum não existe!")
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer)
+
+        let nftTxn = await connectedContract.makeAnEpicNFT()
+        console.log("Vai abrir a carteira agora para pagar o gás...")
+        
+        console.log("Cunhando...espere por favor.")
+        await nftTxn.wait()
+        console.log(`Cunhado, veja a transação: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`)
+        setInterval(() => {
+          document.location.reload(true);
+        }, 1000);
+      } else {
+        console.log("Objeto ethereum não existe!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   // Métodos para Renderizar
   const renderNotConnectedContainer = () => (
-    <button onClick={connectWallet} className="cta-button connect-wallet-button">
-      Conectar Carteira
-    </button>
+    <button
+    onClick={connectWallet}
+    className="cta-button connect-wallet-button"
+  >
+    Conectar carteira
+  </button>
   )
-  
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
   /*
-   * Adicionei um render condicional! Nós não queremos mostrar o Connect to Wallet se já estivermos conectados
+   * Isso roda nossa função quando a página carrega.
    */
+  useEffect(() => {
+    checkIfWalletIsConnected()
+    getTotalNFTsMintedSoFar()
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">Minha Coleção NFT</p>
-          <p className="sub-text">
-            Únicas. Lindas. Descubra a sua NFT hoje.
-          </p>
+          <p className="header gradient-text">Minha Coleção de NFT</p>
+          <p className="sub-text">Exclusivos! Maravilhosos! Únicos! Descubra seu NFT hoje.</p>
+          <div className="sub-text benefit_2">
+              <div style={{ fontSize: "1.5em" }}>
+                {mintTotal}/{TOTAL_MINT_COUNT}
+              </div>
+              NFTs mintados
+          </div>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
+            <button onClick={askContractToMintNft} className="cta-button mint-button">
               Cunhar NFT
             </button>
           )}
@@ -115,10 +191,11 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`feito com ❤️ pela @${TWITTER_HANDLE}`}</a>
+          >{`feito com ❤️ por @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
-  );
-};
-export default App;
+  )
+}
+
+export default App
